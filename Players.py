adf475c9f8479df26c6dd0ac1ed4470d1914ea5f
ExpIPiP1E0 <diagmppp@gmail.com>
2018-09-09 19:11:42 +0900

    Update Players.py

--- Players.py
+++ Players.py
@@ -18,20 +18,5 @@ class RandomPlayer(object):
     
     
 ###############################################################################
-class HumanPlayer(object):
-    '''
-    人間プレイヤー
-    '''
-    def __init__(self,env):
-        self.inner_env=env
-    
-    
-    def play(slef,state,player):
-        valids=self.inner_env.action_space.valid(state,player)
-        #全ての合法手を表示する
-        
-        #入力受付
-        while Ture:
-            a=input()
-            x,y=[int(x) for x in a.split(' ')] #x y形式で入力
-            
+#class HumanPlayer(object):
+#Not implemented yet.
